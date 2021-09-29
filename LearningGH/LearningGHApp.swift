d32f0d6f064f0c2d6d2928884d32fd4324504daf
Isaiah Tullis <micknight@me.com>
2021-09-29 11:59:20 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Isaiah Tullis on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
